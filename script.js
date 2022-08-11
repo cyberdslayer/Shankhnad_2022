eb9aae00f92a73456fce98f296d072b58f75aca3
cyberdslayer <95081360+cyberdslayer@users.noreply.github.com>
2022-08-12 00:37:42 +0530

    FIRST

---
+++ script.js
@@ -0,0 +1,31 @@
+// Navbar Alter
+// function Menu(e){
+//     const list =document.querySelector('ul');
+
+//     e.name === 'menu' ? (e.name = "close ", e.classList.add("fa-xmark"), e.classList.remove("fa-bars"), list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu", e.classList.add("fa-bars"), e.classList.remove("fa-xmark"), list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
+// }
+
+
+// Navbar 
+function Menu(e){
+    const list =document.querySelector('ul');
+
+    e.name === 'menu' ? (e.name = "close ", e.className = "fa-solid fa-bars ", list.classList.add("top-[-80px]", "hidden")) : (e.name = "menu", e.className = "fa-solid fa-xmark ", list.classList.remove("top-[-80px]", "opacity-100", "hidden"))
+}
+
+
+
+// Splide
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var splide = new Splide('.splide', {
+            type: 'loop',
+            perPage: 3,
+            pagination: true,
+        });
+        splide.mount();
+    }); 
+
+
+
+
